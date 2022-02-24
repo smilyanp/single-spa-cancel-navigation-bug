ae4a22e4fd2191c665ff007bb8468c26d0bddb7e
Smilyan Pavlov <smilyan.pavlov@iris.co.uk>
2022-02-24 10:25:16 +0000

    Example cancel navigation issues

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-app';
+
+  @HostListener('window:single-spa:before-routing-event', ['$event.detail'])
+  public beforeRoutingEvent($event: any): void {
+    console.log(
+      '=== history.length in before-routing-event hook ' + history.length
+    );
+    console.log(
+      'new URL($event.newUrl).pathname',
+      new URL($event.newUrl).pathname
+    );
+    if (
+      new URL($event.oldUrl).pathname === '/angular/' &&
+      new URL($event.newUrl).pathname === '/react'
+    ) {
+      if (
+        confirm(
+          'Are you sure you want to navigate? You might lose some data'
+        ) == true
+      ) {
+        console.log("don't cancel navigation");
+      } else {
+        console.log('cancel navigation');
+        $event.cancelNavigation();
+      }
+    }
+  }
 }
